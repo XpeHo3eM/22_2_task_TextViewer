241a2a7e326b2540a2522239e61ac9119825dbf1
XpeHo3eM <npv90@mail.ru>
2021-03-05 12:39:27 +0400

    add comment

--- 22_2_task.cpp
+++ 22_2_task.cpp
@@ -11,19 +11,30 @@ int main()
     file.open(path, std::ios::binary);
     if (file.is_open())
     {
+        // Создаем буферную переменную
         char temp[11];
+
+        // Запоминаем размер буфера с учетом записи 0 в конец
         int sizeForRead = sizeof(temp) - 1;
+
+        // Ставим указатель на последний байт и запоминаем оставшееся количество
         file.seekg(0, std::ios::end);
         int sizeLeft = file.tellg();
+
+        // Ставим указатель обратно на начало
         file.seekg(0, std::ios::beg);
 
         while (sizeLeft > sizeForRead)
         {
             file.read(temp, sizeForRead);
+
+            // Записываем в конец буфера 0 как окончание буфера
             temp[sizeForRead] = 0;
             std::cout << temp;
             sizeLeft -= sizeForRead;
         }
+
+        // Когда осталось байт меньше буфера - записываем остатки
         file.read(temp, sizeLeft);
         temp[sizeLeft] = 0;
         std::cout << temp;
